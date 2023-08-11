2829a32bded0db4ba1d85d480ded55467511df74
Muckki <ckdduq0508@gmail.com>
2023-08-11 17:31:08 +0900

    게시판만들기 초반 / 형태 구성

--- src/com/practice/java/BAM/Main.java
+++ src/com/practice/java/BAM/Main.java
@@ -1,60 +1,72 @@
 package com.practice.java.BAM;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 class Main {
 	public static void main(String[] args) {
-		사람인력관리소 a사람인력관리소 = new 사람인력관리소();
-		
-		a사람인력관리소.add사람("홍길순", 33);
-		// 나이가 33살인 1번째 사람(홍길순)이 추가되었습니다.
-		a사람인력관리소.add사람("홍길동", 20);
-		// 나이가 20살인 2번째 사람(홍길동)이 추가되었습니다.
-		a사람인력관리소.add사람("임꺽정", 30);
-		// 나이가 30살인 3번째 사람(임꺽정)이 추가되었습니다.
-		a사람인력관리소.add사람("나아라", 25);
-		a사람인력관리소.add사람("거짓룰", 27);
-		사람 a사람1 = a사람인력관리소.get사람(1);
-		a사람1.자기소개();
-		// 저는 1번, 홍길순, 33살 입니다.
-		
-		사람 a사람2 = a사람인력관리소.get사람(2);
-		a사람2.자기소개();
-		// 저는 2번, 홍길동, 20살 입니다.
-		
-		사람 a사람3 = a사람인력관리소.get사람(3);
-		a사람3.자기소개();
-		// 저는 3번, 임꺽정, 30살 입니다.
-		사람 a사람4 = a사람인력관리소.get사람(4);
-		a사람4.자기소개();
-		사람 a사람5 = a사람인력관리소.get사람(5);
-		a사람5.자기소개();
-	}
-}
-class 사람인력관리소 {
-	사람[] 사람들;
-	사람인력관리소() {
-		사람들 = new 사람[200];
-	}
-	int 추가된사람_명수;
-	void add사람(String 이름 , int 나이) {
-		int 순서 = 추가된사람_명수 + 1;
-		사람 a사람 = new 사람();
-		a사람.이름 = 이름;
-		a사람.나이 = 나이;
-		a사람.순서 = 순서;
-		System.out.println("나이가 " + a사람.나이 + "살인 " + a사람.순서 +"번째 사람(" + a사람.이름 + ")이 추가되었습니다.");
-		추가된사람_명수 = a사람.순서;
-		사람들[a사람.순서] = a사람;
-	}
-	사람 get사람(int 번호) {
-		return 사람들[번호]; 
+		System.out.println("==프로그램 시작==");
+		Scanner sc = new Scanner(System.in);
+		int 마지막번호 = 0;
+
+		List<게시글> 게시글들 = new ArrayList<>();
+		while (true) {
+			System.out.printf("명령어) ");
+			String command = sc.nextLine();
+			if (command.equals("게시글 리스트")) {
+				if (마지막번호 == 0) {
+					System.out.println("게시글이 없습니다.");
+				} else {
+					System.out.println("번호  /  제목");
+					for (int i = 0; i < 게시글들.size(); i++) {
+						게시글 a게시글 = 게시글들.get(i);
+						System.out.println(" " + a게시글.번호 + "  /  " + a게시글.제목);
+					}
+					while (true) {
+						System.out.println("몇번째 글을 확인하시겠습니까?");
+						System.out.println("번호 : ex)1 or 2 or 뒤로가기");
+						String nextString = sc.nextLine();
+						if (nextString.equals("뒤로가기")) {
+							break;
+						} else {
+							int number = Integer.parseInt(nextString);
+							게시글 b게시글 = 게시글들.get(number - 1);
+							System.out.println("내용 : " + b게시글.내용);
+						}
+					}
+				}
+			} else if (command.equals("게시글 작성")) {
+				int 번호 = 마지막번호 + 1;
+				System.out.printf("제목 : ");
+				String 제목 = sc.nextLine();
+				System.out.printf("내용 : ");
+				String 내용 = sc.nextLine();
+				System.out.println(번호 + "번글이 생성되었습니다.");
+				마지막번호 = 번호;
+				게시글 a게시글 = new 게시글(번호, 제목, 내용);
+
+				게시글들.add(a게시글);
+				continue;
+			} else if (command.equals("exit")) {
+				break;
+			} else {
+				System.out.println("존재하지 않는 명령어 입니다.");
+			}
+		}
+		System.out.println("==프로그램 끝==");
+		sc.close();
 	}
 }
 
-class 사람 {
-	int 나이;
-	String 이름;
-	int 순서;
-	void 자기소개() {
-		System.out.println("저는 "+ 순서 + "번, " + 이름 + ", " + 나이 + "살 입니다.");
+class 게시글 {
+	int 번호;
+	String 제목;
+	String 내용;
+
+	public 게시글(int 번호, String 제목, String 내용) {
+		this.번호 = 번호;
+		this.제목 = 제목;
+		this.내용 = 내용;
 	}
 }
